11d5ac894c16d5d54a4d6b8f0ea21c482b334d5f
ThiagoSantos-hub <thiagogestorbm@gmail.com>
2026-07-01 05:30:45 -0300

    Create index.ts

---
+++ components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './Button'
+export { Input } from './Input'
+export { Modal } from './Modal'
+export { Table } from './Table'
+export type { Column } from './Table'
+export { Card, CardHeader } from './Card'
