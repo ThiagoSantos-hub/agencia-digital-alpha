c32ae8e922d589055fdb2d8178b7815b4a6af4b2
ThiagoSantos-hub <thiagogestorbm@gmail.com>
2026-07-02 15:33:16 -0300

    Update AlphaWidget.tsx

--- components/AlphaWidget.tsx
+++ components/AlphaWidget.tsx
@@ -4,23 +4,23 @@ import { useEffect } from 'react'
 
 export function AlphaWidget() {
   useEffect(() => {
+    // Cria o elemento do widget
+    const widget = document.createElement('elevenlabs-convai')
+    widget.setAttribute('agent-id', 'agent_0101kwhjn4ymf3warnf5k6ktfb4y')
+    document.body.appendChild(widget)
+
+    // Carrega o script
     const script = document.createElement('script')
     script.src = 'https://unpkg.com/@elevenlabs/convai-widget-embed'
     script.async = true
     script.type = 'text/javascript'
     document.head.appendChild(script)
+
     return () => {
-      if (document.head.contains(script)) {
-        document.head.removeChild(script)
-      }
+      if (document.body.contains(widget)) document.body.removeChild(widget)
+      if (document.head.contains(script)) document.head.removeChild(script)
     }
   }, [])
 
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: '<elevenlabs-convai agent-id="agent_0101kwhjn4ymf3warnf5k6ktfb4y"></elevenlabs-convai>',
-      }}
-    />
-  )
+  return null
 }
